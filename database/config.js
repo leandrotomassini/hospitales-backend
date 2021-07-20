c4a77d4d615f89076b9dc88ad6c23da12def6f27
Leandro C. Tomassini <leandroctomassini@gmail.com>
2021-07-19 21:03:28 -0300

    Fin seccion 10 -Auth

--- database/config.js
+++ database/config.js
@@ -1,23 +1,26 @@
-const mongoose = require("mongoose");
+const mongoose = require('mongoose');
 
-const dbConnection = async () => {
-  try {
-    mongoose.connect(
-      process.env.DB_CNN,
-      {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-        useCreateIndex: true,
-      }
-    );
 
-    console.log("DB ONLINE");
-  } catch (error) {
-    console.log(error);
-    throw new Error("Error a la hora de inicar la BD, debes revisar los logs.");
-  }
-};
+const dbConnection = async() => {
+
+    try {
+        await mongoose.connect( process.env.DB_CNN , {
+            useNewUrlParser: true, 
+            useUnifiedTopology: true,
+            useCreateIndex: true
+        });
+
+        console.log('DB Online');
+        
+    } catch (error) {
+        console.log(error);
+        throw new Error('Error a la hora de iniciar la BD ver logs');
+    }
+
+
+}
+
 
 module.exports = {
-  dbConnection,
-};
+    dbConnection
+}
--- index.js
+++ index.js
@@ -1,27 +1,30 @@
-require("dotenv").config();
+require('dotenv').config();
 
-const express = require("express");
-const cors = require("cors");
+const express = require('express');
+const cors = require('cors');
 
-const { dbConnection } = require("./database/config");
+const { dbConnection } = require('./database/config');
 
-// Crear el servidor express
+// Crear el servidor de express
 const app = express();
 
 // Configurar CORS
-app.use(cors());
+app.use( cors() );
+
+// Lectura y parseo del body
+app.use( express.json() );
 
 // Base de datos
 dbConnection();
 
+
 // Rutas
-app.get("/", (req, res) => {
-  res.json({
-    ok: true,
-    msg: "Hola mundo",
-  });
+app.use( '/api/usuarios', require('./routes/usuarios') );
+app.use( '/api/login', require('./routes/auth') );
+
+
+
+app.listen( process.env.PORT, () => {
+    console.log('Servidor corriendo en puerto ' + process.env.PORT );
 });
 
-app.listen(process.env.PORT, () => {
-  console.log("Servidor corriendo en puerto " + 3000);
-});
